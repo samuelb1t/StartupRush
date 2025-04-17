78e93328d93701654e212d55722d2043dac89d5a
Samuel <samuel.bitdinger@gmail.com>
2025-04-17 17:07:00 -0300

    inicio do projeto

---
+++ back-end/src/test/java/com/example/StartupRush/StartupRushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StartupRush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupRushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
